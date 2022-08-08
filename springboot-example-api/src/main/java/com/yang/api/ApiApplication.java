2f91d6a93c822e6511dca15687bc3a96d0381e4e
小酱 <57653734+lmy1218@users.noreply.github.com>
2022-08-08 09:56:39 +0800

    fix

--- springboot-example-api/src/main/java/com/yang/api/ApiApplication.java
+++ springboot-example-api/src/main/java/com/yang/api/ApiApplication.java
@@ -2,6 +2,7 @@ package com.yang.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @EnableAsync
+@EnableCaching(proxyTargetClass = true)
 public class ApiApplication {
 
     public static void main(String[] args){
